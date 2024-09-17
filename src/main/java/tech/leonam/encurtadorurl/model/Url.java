54ab1e20fc964318662c8dfeee1f875e571af451
usuario <u@u.c>
2024-09-17 12:20:07 -0300

    Replace dos imports

--- src/main/java/tech/leonam/encurtadorurl/model/Url.java
+++ src/main/java/tech/leonam/encurtadorurl/model/Url.java
@@ -1,6 +1,10 @@
 package tech.leonam.encurtadorurl.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
